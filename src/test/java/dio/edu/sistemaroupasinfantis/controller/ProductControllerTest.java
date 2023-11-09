98cc279ea9f06d925c1f27be8c389e2634a808a7
chrissperb <chrissperb@gmail.com>
2023-11-09 19:37:33 -0300

    Continuação da implementação dos testes unitários.

--- src/test/java/dio/edu/sistemaroupasinfantis/controller/ProductControllerTest.java
+++ src/test/java/dio/edu/sistemaroupasinfantis/controller/ProductControllerTest.java
@@ -1,40 +1,18 @@
 package dio.edu.sistemaroupasinfantis.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dio.edu.sistemaroupasinfantis.ApplicationTests;
-import dio.edu.sistemaroupasinfantis.domain.model.Product;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
 
 class ProductControllerTest extends ApplicationTests {
 
     @Test
-    void findProductByIdTest() throws IOException {
-        //Context
-        ObjectMapper objectMapper = new ObjectMapper();
-        Product[] products = new Product[7];
+    void createProductTest() {
+        // Context
 
-        for (int i = 0; i < 7; i++) {
-            String jsonFilePath = "product" + (i + 1) + ".json";
-            Product product = objectMapper.readValue(new File(jsonFilePath), Product.class);
-            products[i] = product;
-        }
 
-        //Action
-        for (Product product : products) {
-            System.out.println("Descrição: " + product.getDescription());
-            System.out.println("Código: " + product.getCode());
-            System.out.println("Preço: " + product.getPrice());
-    }
+        // Action
 
-        //@Test
-        // void createProductTest() {
-        //Context
-
-        //Action
-
-        //
+        // Verification
     }
 }
